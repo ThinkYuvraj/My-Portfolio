a31f1dc1bb0a78e9f9625864f5df74d0bf01dab3
Yuvraj <thinkyuvraj@gmail.com>
2026-06-20 18:40:45 +0530

    Initial portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+  extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+      sans: ["Plus Jakarta Sans", "sans-serif"],
+    },
+
+    colors: {
+      primary: "#a855f7",
+      dark: "#000000",
+      card: "#050505",
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },
+  },
+},
+  plugins: [],
+}
